09834ea80195534111901c90fa899acdd38afba6
OzamaGenova <genovaoz@yandex.ru>
2023-12-14 20:19:47 +0400

    add orders part

--- LK/lk.js
+++ LK/lk.js
@@ -1,2 +1,19 @@
-// TUT bal a tam ne bal 
-// crutoi chlen
+function showTab(tabName) {
+    var tabs = document.querySelectorAll(".tab-content");
+    tabs.forEach(function(tab) {
+      if (tab.id === tabName) {
+        tab.style.display = "block";
+      } else {
+        tab.style.display = "none";
+      }
+    });
+
+    var links = document.querySelectorAll(".sidebar a");
+    links.forEach(function(link) {
+      if (link.getAttribute("href") === "#" + tabName) {
+        link.parentElement.classList.add("active");
+      } else {
+        link.parentElement.classList.remove("active");
+      }
+    });
+  }
