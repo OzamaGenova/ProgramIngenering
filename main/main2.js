243f6287299901eff278c1699f8ad9c0266725f8
OzamaGenova <genovaoz@yandex.ru>
2023-12-14 16:17:25 +0400

    fix more problem layout

--- main/main2.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    var myVar = "Коля";
-    console.log(myVar); // -> "Коля" — myVar доступна внутри функции.
-  }
-  console.log(myVar); // ReferenceError, myVar недоступна снаружи функции.
